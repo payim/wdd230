0b9a3e0d719b7c1619f48ff3dd1b5c26ab5568d4
payim2022 <ayi12001@byui.edu>
2023-05-08 11:55:14 -0600

    first psot

---
+++ scripts/getDate.js
@@ -0,0 +1,2 @@
+requestAnimationFrame(() => { document.querySelector("#year_container").innerHTML = new Date().getFullYear() });
+requestAnimationFrame(() => { document.querySelector("#lastModified").innerHTML = "Last Modification: " + document.lastModified });

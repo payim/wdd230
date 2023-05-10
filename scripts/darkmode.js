570d3a66befc3161dbff3163fe96b92dc23c0007
payim2022 <ayi12001@byui.edu>
2023-05-09 18:11:51 -0600

    dark 1

---
+++ scripts/darkmode.js
@@ -0,0 +1,14 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#e0e0e0";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});

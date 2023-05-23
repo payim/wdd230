d444d368edbee9b241e5fe49847a219432101529
payim2022 <ayi12001@byui.edu>
2023-05-23 16:08:40 -0600

    new11

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const themeToggle = document.querySelector("#theme-toggle");
+const mobileToggle = document.querySelector("#mobile-toggle");
+const visitsCounter = document.querySelector("#visit_counter");
+
+themeToggle.addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("t-dark");
+    themeToggle.querySelectorAll(".icon").forEach((icon) => {
+        icon.classList.toggle("active");
+    });
+});
+
+mobileToggle.addEventListener("click", () => {
+    mobileToggle.querySelectorAll(".icon").forEach((icon) => {
+        icon.classList.toggle("active");
+    });
+    mobileToggle.parentElement.querySelector(".contents").classList.toggle("open");
+});
+
+let totalVisits = Number(window.localStorage.getItem("totalVisits")) || 0;
+
+if (totalVisits !== 0) {
+    visitsCounter.textContent = totalVisits;
+} else {
+    visitsCounter.textContent = `You're the first! 🥳 Welcome!`;
+}
+
+totalVisits++;
+
+localStorage.setItem("totalVisits", totalVisits);
